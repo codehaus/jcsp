f081aea679fa546f9f86e5acb38f8d11025dbf6f
nccb2 <nccb2@5ee8cc34-b920-0410-bb03-fa7ad842d588>
2007-02-09 12:24:16 +0000

    PHW: added to the documentation, and added IllegalArgumentExceptions to some of the methods
    git-svn-id: http://projects.cs.kent.ac.uk/projects/jcsp/svn/jcsp/trunk@84 5ee8cc34-b920-0410-bb03-fa7ad842d588

--- src/org/jcsp/lang/Barrier.java
+++ src/org/jcsp/lang/Barrier.java
@@ -34,7 +34,7 @@ import java.io.Serializable;
  * This enables <I>barrier</I> synchronisation between a set of processes.
  * <P>
  * <A HREF="#constructor_summary">Shortcut to the Constructor and Method Summaries.</A>
- *
+ * 
  * <H2>Description</H2>
  * A channel is a CSP <I>event</I> in which only two processes (the reader and
  * the writer) synchronise.  A barrier is a CSP <I>event</I> in which any number of
@@ -42,6 +42,7 @@ import java.io.Serializable;
  * blocked until <I>all</I> processes associated with that barrier have synchronised.
  * A process may not back off an attempted synchronisation - i.e. barriers cannot be
  * used as guards in an {@link Alternative}.
+ * Applications needing that capability should use an {@link AltingBarrier}.
  * <P>
  * A process network may contain many barriers - each being associated with a different
  * subset of processes.  These subsets may overlap and change at run-time.  JCSP does not
@@ -50,12 +51,12 @@ import java.io.Serializable;
  * thought worthwhile.  Associating a barrier with its correct set of processes is
  * a design issue and it is left to the designer to ensure correct useage.
  * <P>
- * <I>Note:</I> this notion of barrier corresponds to
- * the <A HREF="http://www.hensa.ac.uk/parallel/occam/projects/occam-for-all/hlps/">EVENT</A>
- * synchronisation primitive added to
- * the <A HREF="http://www.hensa.ac.uk/parallel/occam/projects/occam-for-all/kroc/">KRoC</A>
- * <B>occam</B> language system.
- *
+ * <I>Note:</I> this notion of barrier corresponds to the
+ * <A HREF="http://frmb.org/occ21-extensions-clean.html#barrier"><tt>BARRIER</tt></A>
+ * synchronisation primitive added to the
+ * <A HREF="http://www.cs.kent.ac.uk/projects/ofa/kroc/">KRoC</A>
+ * <A HREF="http://occam-pi.org/"><B>occam-pi</B></A> language system.
+ * 
  * <H3>Deterministic Barriers</H3>
  * If the set of processes associated with a barrier remains fixed, barrier synchronisation
  * introduces no <I>non-determinism</I>.  So, a parallel system made up of processes,
@@ -126,7 +127,7 @@ import java.io.Serializable;
  * This work takes a different amount of time in each cycle and varies from player to player.
  * At the end of each piece of work, each player waits for all its colleagues before continuing
  * its next cycle.
- *
+ * 
  * <H3><A NAME="ND-barrier">Non-Deterministic Barriers</H3>
  * A process may choose at any time to {@link #enroll enroll} or {@link #resign resign} from
  * any barrier it can see.  It should not, of course, <TT>enroll</TT> on a barrier with which
@@ -313,8 +314,8 @@ import java.io.Serializable;
  * the barrier synchronisation.  For the <TT>Worker</TT>, modify its <I>working</I> loop:
  * <PRE>
  *   for (int i = 0; i < nWorkUnits; i++) {
- *     barrier.sync ();          // wait for everyone
- *     barrier.sync ();          // wait for the Timekeeper to report
+ *     barrier.sync ();                     // wait for everyone
+ *     barrier.sync ();                     // wait for the Timekeeper to report
  *     System.out.println (working);
  *     tim.sleep (random.nextInt (maxWork));
  *   }
@@ -323,13 +324,13 @@ import java.io.Serializable;
  * <PRE>
  *   while (true) {
  *     tim.after (timeout);
- *     barrier.sync ();          // wait for everyone
+ *     barrier.sync ();                     // wait for everyone
  *     System.out.println ("[" + (tim.read () - timeout) + "]");
- *     barrier.sync ();          // let the Workers get going
+ *     barrier.sync ();                     // let the Workers get going
  *     timeout += interval;
  *   }
  * </PRE>
- *
+ * 
  * <H3>Overheads</H3>
  * Free use of additional synchronisations to gain special control (such as in the above)
  * depends on the overheads being not so great as to render that control pointless.
@@ -356,9 +357,10 @@ import java.io.Serializable;
  * has (sub-microsecond) unit time costs for <I>all</I> its operations, including
  * the <I>final</I> <TT>sync</TT>.  Future work on JCSP may look towards this standard.]
  *
+ * @see org.jcsp.lang.AltingBarrier
  * @see org.jcsp.lang.Bucket
- * @see org.jcsp.lang.One2OneChannelImpl
- *
+ * @see org.jcsp.lang.Alternative
+ * 
  * @author P.H.Welch
  */
 
@@ -394,11 +396,22 @@ public class Barrier implements Serializable
 
   /**
    * Construct a barrier (initially) associated with <TT>nEnrolled</TT> processes.
+   * It is the responsibility of the constructing process to pass this
+   * (by constructor or <tt>set</tt> method) to each process that will be
+   * synchronising on the barrier, <i>before</i> firing up those processes.
+   * <p>
    *
    * @param nEnrolled the number of processes (initially) associated with this barrier.
+   * <p>
+   * 
+   * @throws IllegalArgumentException if <tt>nEnrolled</tt> < <tt>0</tt>.
    */
-    public Barrier(final int nEnrolled)
-    {
+  public Barrier (final int nEnrolled) {
+    if (nEnrolled < 0) {
+      throw new IllegalArgumentException (
+        "*** Attempt to set a negative enrollment on a barrier\n"
+      );
+    }
     this.nEnrolled = nEnrolled;
     countDown = nEnrolled;
 //System.out.println ("Barrier.constructor : " + nEnrolled + ", " + countDown);
@@ -407,8 +420,15 @@ public class Barrier implements Serializable
   /**
    * Reset this barrier to be associated with <TT>nEnrolled</TT> processes.
    * This must only be done at a time when no processes are active on the barrier.
+   * It is the responsibility of the invoking process to pass this barrier
+   * (by constructor or <tt>set</tt> method) to each process that will be
+   * synchronising on the barrier, <i>before</i> firing up those processes.
+   * <p>
    *
    * @param nEnrolled the number of processes reset to this barrier.
+   * <p>
+   * 
+   * @throws IllegalArgumentException if <tt>nEnrolled</tt> < <tt>0</tt>.
    */
   public void reset (final int nEnrolled) {
     if (nEnrolled < 0) {
@@ -436,17 +456,17 @@ public class Barrier implements Serializable
         try {
           boolean spuriousCycle = evenOddCycle;
           barrierLock.wait ();
-	      while (spuriousCycle == evenOddCycle) {
-	        if (Spurious.logging) {
-	          SpuriousLog.record (SpuriousLog.BarrierSync);
-	        }
-	        barrierLock.wait (); 
+	  while (spuriousCycle == evenOddCycle) {
+	    if (Spurious.logging) {
+	      SpuriousLog.record (SpuriousLog.BarrierSync);
+	    }
+	    barrierLock.wait ();
           }	  
         }
         catch (InterruptedException e) {
           throw new ProcessInterruptedException (
-	        "*** Thrown from Barrier.sync ()\n" + e.toString ()
-	      );
+	    "*** Thrown from Barrier.sync ()\n" + e.toString ()
+	  );
         }
       } else {
         countDown = nEnrolled;
@@ -458,7 +478,31 @@ public class Barrier implements Serializable
   }
 
   /**
-   * Associate the invoking process with this barrier.
+   * A process may enroll only if it is resigned.
+   * A re-enrolled process may resume offering to synchronise on this barrier
+   * (until a subsequent {@link #resign resign}).
+   * Other processes cannot complete the barrier (represented by this front-end)
+   * without participation by the re-enrolled process.
+   * <p>
+   * <i>Note:</i> timing re-enrollment on a barrier usually needs some care.
+   * If the barrier is being used for synchronising phases of execution between
+   * a set of processes, it is crucial that re-enrollment occurs in
+   * an appropriate <i>(not arbitrary)</i> phase.
+   * If the trigger for re-enrollment comes from another enrolled process,
+   * that process should be in such an appropriate phase.
+   * The resigned process should re-enroll and, then, acknowledge the trigger.
+   * The triggering process should wait for that acknowledgement.
+   * If the decision to re-enroll is internal (e.g. following a timeout),
+   * a <i>buddy</i> process, enrolled on the barrier, should be asked to provide
+   * that trigger when in an appropriate phase.
+   * The <i>buddy</i> process, perhaps specially built just for this purpose, polls
+   * a service channel for that question when in that phase.
+   * </p>
+   * <p>
+   * <i>Warning:</i> the rule in the first sentence above is
+   * the responsibility of the designer -- it is not checked by
+   * implementation.
+   * If not honoured, things will go wrong.
    */
   public void enroll () {
     synchronized (barrierLock) {
@@ -469,11 +513,27 @@ public class Barrier implements Serializable
   }
 
   /**
-   * Disassociate the invoking process from this barrier.
-   * Note that if the resigner is the last process associated with the barrier
-   * not to have invoked a {@link #sync sync}, its resignation <I>completes</I>
-   * the barrier (as though it has invoked a <TT>sync</TT>) and releases all
-   * the remaining associated processes.
+   * A process may resign only if it is enrolled.
+   * A resigned process may not offer to synchronise on this barrier
+   * (until a subsequent {@link #enroll enroll}).
+   * Other processes can complete the barrier (represented by this front-end)
+   * without participation by the resigned process.
+   * <p>
+   * Unless <i>all</i> processes synchronising on this barrier terminate in
+   * the same phase, it is usually appropriate for a terminating process
+   * to <i>resign</i> first.  Otherwise, its sibling processes will never be
+   * able to complete another synchronisation.
+   * </p>
+   * <p>
+   * <i>Warning:</i> the rules in the first two sentences above are
+   * the responsibility of the designer -- they are not checked by
+   * implementation.
+   * If not honoured, things will go wrong.
+   * </p>
+   * <p>
+   * 
+   * @throws BarrierError if not enrolled <i>(but this is not always detected)</i>.
+   * 
    */
   public void resign () {
     synchronized (barrierLock) {
