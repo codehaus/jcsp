1dbcc87d01f6e1a7eeb8071b11408151c139a985
nccb2 <nccb2@5ee8cc34-b920-0410-bb03-fa7ad842d588>
2007-09-25 14:50:12 +0000

    Added Object versions of the symmetric int channels
    git-svn-id: http://projects.cs.kent.ac.uk/projects/jcsp/svn/jcsp/trunk@154 5ee8cc34-b920-0410-bb03-fa7ad842d588

---
+++ src/org/jcsp/lang/AltingChannelOutput.java
@@ -0,0 +1,95 @@
+
+  /*************************************************************************
+  *                                                                        *
+  *  JCSP ("CSP for Java") libraries                                       *
+  *  Copyright (C) 1996-2001 Peter Welch and Paul Austin.                  *
+  *                                                                        *
+  *  This library is free software; you can redistribute it and/or         *
+  *  modify it under the terms of the GNU Lesser General Public            *
+  *  License as published by the Free Software Foundation; either          *
+  *  version 2.1 of the License, or (at your option) any later version.    *
+  *                                                                        *
+  *  This library is distributed in the hope that it will be useful,       *
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+  *  Lesser General Public License for more details.                       *
+  *                                                                        *
+  *  You should have received a copy of the GNU Lesser General Public      *
+  *  License along with this library; if not, write to the Free Software   *
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,  *
+  *  USA.                                                                  *
+  *                                                                        *
+  *  Author contact: P.H.Welch@ukc.ac.uk                                   *
+  *                                                                        *
+  *************************************************************************/
+
+package org.jcsp.lang;
+
+//{{{  javadoc
+/**
+ * This extends {@link Guard} and {@link ChannelOutputInt}
+ * to enable a process
+ * to choose between many integer output (and other) events.
+ * <H2>Description</H2>
+ * <TT>AltingChannelOutputInt</TT> extends {@link Guard} and {@link ChannelOutputInt}
+ * to enable a process
+ * to choose between many integer output (and other) events.  The methods inherited from
+ * <TT>Guard</TT> are of no concern to users of this package.
+ * </P>
+ * <H2>Example</H2>
+ * <PRE>
+ * import jcsp.lang.*;
+ * <I></I>
+ * public class AltingIntOutputExample implements CSProcess {
+ * <I></I>
+ *   private final AltingChannelOutputInt out0, out1;
+ *   <I></I>
+ *   public AltingIntOutputExample (final AltingChannelOutputInt out0,
+ *                                  final AltingChannelOutputInt out1) {
+ *     this.out0 = out0;
+ *     this.out1 = out1;
+ *   }
+ * <I></I>
+ *   public void run () {
+ * <I></I>
+ *     final Guard[] altChans = {out0, out1};
+ *     final Alternative alt = new Alternative (altChans);
+ * <I></I>
+ *     while (true) {
+ *       switch (alt.select ()) {
+ *         case 0:
+ *           out0.write (0);
+ *           System.out.println ("out0 written");
+ *         break;
+ *         case 1:
+ *           out1.write (1);
+ *           System.out.println ("out1 written" + out1.read ());
+ *         break;
+ *       }
+ *     }
+ * <I></I>
+ *   }
+ * <I></I>
+ * }
+ * </PRE>
+ *
+ * @see jcsp.lang.Guard
+ * @see jcsp.lang.Alternative
+ * @author P.D.Austin and P.H.Welch
+ */
+//}}}
+
+public abstract class AltingChannelOutput extends Guard implements ChannelOutput {
+
+  // nothing alse to add
+
+  /**
+   * Returns whether the receiver is committed to read from this channel.
+   * <P>
+   * <I>Note: if this returns true, you must commit to write down this channel.</I>
+   *
+   * @return state of the channel.
+   */
+  public abstract boolean pending ();
+
+}
---
+++ src/org/jcsp/lang/AltingChannelOutputSymmetricImpl.java
@@ -0,0 +1,70 @@
+
+  /*************************************************************************
+  *                                                                        *
+  *  JCSP ("CSP for Java") libraries                                       *
+  *  Copyright (C) 1996-2001 Peter Welch and Paul Austin.                  *
+  *                                                                        *
+  *  This library is free software; you can redistribute it and/or         *
+  *  modify it under the terms of the GNU Lesser General Public            *
+  *  License as published by the Free Software Foundation; either          *
+  *  version 2.1 of the License, or (at your option) any later version.    *
+  *                                                                        *
+  *  This library is distributed in the hope that it will be useful,       *
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+  *  Lesser General Public License for more details.                       *
+  *                                                                        *
+  *  You should have received a copy of the GNU Lesser General Public      *
+  *  License along with this library; if not, write to the Free Software   *
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,  *
+  *  USA.                                                                  *
+  *                                                                        *
+  *  Author contact: P.H.Welch@ukc.ac.uk                                   *
+  *                                                                        *
+  *************************************************************************/
+
+package org.jcsp.lang;
+
+class AltingChannelOutputSymmetricImpl extends AltingChannelOutput
+  implements MultiwaySynchronisation {
+
+  private final AltingBarrier ab;
+
+  private final ChannelOutput out;
+
+  private boolean syncDone = false;
+  
+  public AltingChannelOutputSymmetricImpl (
+    AltingBarrier ab, ChannelOutput out) {
+    this.ab = ab;
+    this.out = out;
+  }
+
+  boolean enable (Alternative alt) {
+    syncDone = ab.enable (alt);
+    return syncDone;
+  }
+
+  boolean disable () {
+    syncDone = ab.disable ();
+    return syncDone;
+  }
+
+  public void write (Object o) {
+    if (!syncDone) ab.sync ();
+    syncDone = false;
+    out.write (o);
+  }
+
+  public boolean pending () {
+    syncDone = ab.poll (10);
+    return syncDone;
+  }
+
+  public void poison(int strength) {
+	out.poison(strength);
+  }
+  
+  
+
+}
