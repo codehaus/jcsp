ef77df0558d050bbd3a8c06c699c1b17094e8a66
phw <phw@5ee8cc34-b920-0410-bb03-fa7ad842d588>
2008-11-25 20:11:12 +0000

    Copied Flasher stuff from src/demos/flasher to doc-ex/jcsp/awt. Gives them twice ... no matter!
    git-svn-id: http://projects.cs.kent.ac.uk/projects/jcsp/svn/jcsp/trunk@232 5ee8cc34-b920-0410-bb03-fa7ad842d588

--- doc-ex/jcsp/awt/FlasherControl.java
+++ doc-ex/jcsp/awt/FlasherControl.java
@@ -1,3 +1,32 @@
+
+    //////////////////////////////////////////////////////////////////////
+    //                                                                  //
+    //  JCSP ("CSP for Java") Libraries                                 //
+    //  Copyright (C) 1996-2008 Peter Welch and Paul Austin.            //
+    //                2001-2004 Quickstone Technologies Limited.        //
+    //                                                                  //
+    //  This library is free software; you can redistribute it and/or   //
+    //  modify it under the terms of the GNU Lesser General Public      //
+    //  License as published by the Free Software Foundation; either    //
+    //  version 2.1 of the License, or (at your option) any later       //
+    //  version.                                                        //
+    //                                                                  //
+    //  This library is distributed in the hope that it will be         //
+    //  useful, but WITHOUT ANY WARRANTY; without even the implied      //
+    //  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         //
+    //  PURPOSE. See the GNU Lesser General Public License for more     //
+    //  details.                                                        //
+    //                                                                  //
+    //  You should have received a copy of the GNU Lesser General       //
+    //  Public License along with this library; if not, write to the    //
+    //  Free Software Foundation, Inc., 59 Temple Place, Suite 330,     //
+    //  Boston, MA 02111-1307, USA.                                     //
+    //                                                                  //
+    //  Author contact: P.H.Welch@kent.ac.uk                             //
+    //                                                                  //
+    //////////////////////////////////////////////////////////////////////
+
+
 import org.jcsp.lang.*;
 import org.jcsp.awt.*;
 import java.awt.*;
@@ -25,9 +54,10 @@ public class FlasherControl implements CSProcess {
     }
     public void configure (java.applet.Applet applet) {
       applet.setBackground (colour);
+      applet.repaint ();
     }
   }
-
+    
   public void run () {
 
     final Random random = new Random ();
@@ -78,6 +108,7 @@ public class FlasherControl implements CSProcess {
         break;
 
         case TIMER:
+          // System.out.println ("FlasherControl: tick");
           timeout += period;
           tim.setAlarm (timeout);
           index = 1 - index;
--- doc-ex/jcsp/awt/FlasherNetworkSSD.java
+++ doc-ex/jcsp/awt/FlasherNetworkSSD.java
@@ -30,13 +30,13 @@
 import org.jcsp.lang.*;
 import org.jcsp.awt.*;
 
-public class FlasherNetwork implements CSProcess {
+public class FlasherNetworkSSD implements CSProcess {
 
   final private long period;
   final private ActiveApplet activeApplet;
 
-  public FlasherNetwork (final long period,
-                         final ActiveApplet activeApplet) {
+  public FlasherNetworkSSD (final long period,
+                            final ActiveApplet activeApplet) {
     this.period = period;
     this.activeApplet = activeApplet;
   }
@@ -45,14 +45,52 @@ public class FlasherNetwork implements CSProcess {
 
     final One2OneChannel mouseEvent = Channel.one2one ();
     final One2OneChannel appletConfigure = Channel.one2one ();
+    final One2OneChannelInt stopStart = Channel.one2oneInt ();
+    final One2OneChannelInt destroy = Channel.one2oneInt ();
+    final One2OneChannelInt destroyAck = Channel.one2oneInt ();
 
     activeApplet.addMouseEventChannel (mouseEvent.out());
     activeApplet.setConfigureChannel (appletConfigure.in());
+    activeApplet.setStopStartChannel (stopStart.out());
+    activeApplet.setDestroyChannels (destroy.out(), destroyAck.in());
+    // activeApplet.setDestroyChannels (destroy.out(), destroyAck.in(), -1);
 
     new Parallel (
       new CSProcess[] {
         activeApplet,
-        new FlasherControl (period, mouseEvent.in(), appletConfigure.out())
+        new FlasherControl (period, mouseEvent.in(), appletConfigure.out()),
+        new CSProcess () {
+          public void run () {
+            while (true) {
+              switch (stopStart.in().read ()) {
+                case ActiveApplet.STOP:
+                  System.out.println ("FlasherNetworkSSD: ActiveApplet.STOP received");
+                break;
+                case ActiveApplet.START:
+                  System.out.println ("FlasherNetworkSSD: ActiveApplet.START received");
+                break;
+                default:
+                  System.out.println ("FlasherNetworkSSD: ActiveApplet.<not STOP/START> received");
+                break;
+              }
+            }
+          }
+        },
+        new CSProcess () {
+          public void run () {
+            while (true) {
+              switch (destroy.in().read ()) {
+                case ActiveApplet.DESTROY:
+                  System.out.println ("FlasherNetworkSSD: ActiveApplet.DESTROY received");
+                  destroyAck.out().write (0);
+                break;
+                default:
+                  System.out.println ("FlasherNetworkSSD: ActiveApplet.<not DESTROY> received");
+                break;
+              }
+            }
+          }
+        }
       }
     ).run ();
 
