4ae59cd43d278072c7db9ee6b17051fa3d7204e6
d.n.warren <d.n.warren@5ee8cc34-b920-0410-bb03-fa7ad842d588>
2010-03-11 17:15:59 +0000

    git-svn-id: http://projects.cs.kent.ac.uk/projects/jcsp/svn/jcsp/branches/dnw3_altbar@303 5ee8cc34-b920-0410-bb03-fa7ad842d588

--- src/org/jcsp/lang/GuardGroup.java
+++ src/org/jcsp/lang/GuardGroup.java
@@ -12,6 +12,7 @@ public class GuardGroup extends Guard {
 	//{{{ fields
 	public AltableBarrier selectedBarrier;
 	public Alternative parent;
+	public BarrierFace bf;
 
 	public AltableBarrier[] guards;
 	public Object lock;
@@ -24,6 +25,7 @@ public class GuardGroup extends Guard {
 		selectedBarrier = null;
 		parent = null;
 		lock = null;
+		bf = null;
 	}
 	//}}}
 	
@@ -31,8 +33,12 @@ public class GuardGroup extends Guard {
 	//{{{ public boolean enable(Alternative alt)
 	boolean enable(Alternative alt) {
 		claimLock();
-
 		parent = alt;
+
+		createBarrierFace(); //create BarrierFace if neccesary
+
+		// select a barrier between top and bottomIndexes
+		AltableBarrier ab = selectBarrier();
 
 		return false;
 	}
@@ -41,7 +47,10 @@ public class GuardGroup extends Guard {
 	boolean disable() {
 
 		// do other stuff
+		removeBarrierFace(); //remove BarrierFace if this is last Guard
+					//group
 		
+		parent = null;		
 		releaseLock();
 		return false;
 	}
@@ -56,6 +65,54 @@ public class GuardGroup extends Guard {
 	//{{{ private void releaseLock()
 	private void releaseLock(){}
 	//}}}
+	//{{{ private void createBarrierFace()
+	private void getBarrierFace() {
+		// get the BarrierFace associated with the parent ALT
+		bf = BarrierFace.faces.get(parent);
+
+		if (bf == null) {
+			// new BarrierFace needs to be created
+			bf = BarrierFace(parent);
+			bf.topIndex = 0;
+			bf.bottomIndex = 0;
+		} else {
+			bf.topIndex = 0;
+			bf.bottomIndex++;
+		}
+	}
 	//}}}
+	//{{{ private void removeBarrierFace()
+	private void removeBarrierFace() {
+		bf.bottomIndex--;
+		
+		if (bf.bottomIndex < 0) {
+			// no more GuardGroups to disable in ALT, dispose of bf
+			bf.dispose();
+		}
+		
+		bf = null;
+	}
+	//}}}
+	private AltableBarrier selectBarrier() {
+		Vector guardGroups = bf.guardGroups();
+		
+		for (int i = bf.topIndex; i <= bf.bottomIndex; i++) {
+			GuardGroup gg = (GuardGroup) guardGroups.get(i);
+
+			AltableBarrier ab = selectBarrier(gg);
+
+			if (ab != null) {
+				bf.topIndex = i;
+				return ab;
+			}
+		}
+		return null;
+	}
+
+	private AltableBarrier selectBarrier (GuardGroup gg) {
+		
+	}
+	//}}}
+	
 }
 //}}}
