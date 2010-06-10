3b9d62cbe41013efe02f92047d18dd769cf71af0
d.n.warren <d.n.warren@5ee8cc34-b920-0410-bb03-fa7ad842d588>
2010-06-10 16:33:08 +0000

    Added some more classes to SampleProcesses.java ... Also removed the .class files from the repository.
    git-svn-id: http://projects.cs.kent.ac.uk/projects/jcsp/svn/jcsp/branches/dnw3_altbar@327 5ee8cc34-b920-0410-bb03-fa7ad842d588

--- src/org/jcsp/demos/altableBarriers/SampleProcesses.java
+++ src/org/jcsp/demos/altableBarriers/SampleProcesses.java
@@ -2,6 +2,7 @@
 package org.jcsp.demos.altableBarriers;
 
 import org.jcsp.lang.*;
+import java.io.*;
 //}}}
 
 //{{{ public class SampleProcesses
@@ -10,14 +11,28 @@ public class SampleProcesses {
 	public static CSProcess priProc(AltableBarrierBase[] bars) {
 		return (new PriorityProcess(bars));
 	}
+
+	public static CSProcess arbProc(AltableBarrierBase[] bars) {
+		return (new ArbitraryProcess(bars));
+	}
+	
+	public static CSProcess waitProc(AltableBarrierBase base,
+	  BufferedReader in) {
+		return (new WaitingProcess(base, in));
+	}
+
+	public static CSProcess kybProc(AltableBarrierBase base) {
+		return (new WaitingProcess(base));
+	}
 }
 //}}}
-//{{{ class PriorityProcess extends CSProcess
+//{{{ class PriorityProcess implments CSProcess
 class PriorityProcess implements CSProcess {
 
 	Alternative alt;
 	GuardGroup[] ggs;
 
+	//{{{ PriorityProcess (AltableBarrierBase[] barriers)
 	PriorityProcess(AltableBarrierBase[] barriers) {
 		GuardGroup[] ggs = new GuardGroup[barriers.length];
 		for (int i = 0; i < ggs.length; i++) {
@@ -27,15 +42,83 @@ class PriorityProcess implements CSProcess {
 		alt = new Alternative(ggs);
 		this.ggs = ggs;
 	}
+	//}}}
 
+	//{{{ public void run()
 	public void run() {
 		while (true) {
 			int index = alt.priSelect();
 			System.out.println("index = " + index);
 			GuardGroup gg = (GuardGroup) ggs[index];
-			AltableBarrier ab = (AltableBarrier) gg.lastSynchronised();
+			AltableBarrier ab = gg.lastSynchronised();
 			System.out.println(ab);
 		}
 	}
+	//}}}
+}
+//}}}
+//{{{ class ArbitraryProcess implements CSProcess
+class ArbitraryProcess implements CSProcess {
+
+	Alternative alt;
+	GuardGroup gg;
+
+	//{{{ ArbitraryProcess (AltableBarrierBase[] barriers)
+	ArbitraryProcess (AltableBarrierBase[] barriers) {
+		AltableBarrier[] abs = new AltableBarrier[barriers.length];
+		for (int i = 0; i < abs.length; i++) {
+			AltableBarrier ab = new AltableBarrier(barriers[i]);
+			abs[i] = ab;
+		}
+		gg = new GuardGroup(abs);
+		alt = new Alternative(new Guard[] {gg});
+	}
+	//}}}
+
+	//{{{ public void run() 
+	public void run() {
+		while (true) {
+			int index = alt.priSelect();
+			AltableBarrier ab = gg.lastSynchronised();
+
+			System.out.println(ab);
+		}
+	}
+	//}}}
+}
+//}}}
+
+//{{{ class WaitingProcess implements CSProcess
+class WaitingProcess implements CSProcess {
+
+	BufferedReader in;
+	AltableBarrier bar;
+
+	//{{{ WaitingProcess (AltableBarrierBase base, BufferedReader in)
+	WaitingProcess (AltableBarrierBase base) {
+		this (base, new BufferedReader(
+		  new InputStreamReader (System.in) 
+		));
+	}
+	WaitingProcess (AltableBarrierBase base, BufferedReader in) {
+		this.in = in;
+		bar = new AltableBarrier(base, ABConstants.UNPREPARED);
+	}
+	//}}}	
+
+	//{{{ public void run()
+	public void run() {
+		GuardGroup gg = new GuardGroup(new AltableBarrier[] {bar});
+		Alternative alt = new Alternative(new Guard[] {gg});
+		while (true) {
+			try {
+				in.read();
+			} catch (IOException e) {}
+			System.out.println("Got input");
+			alt.priSelect();
+			System.out.println("Barrier synced");
+		}
+	}
+	//}}}
 }
 //}}}
