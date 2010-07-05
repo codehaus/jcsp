ce2889b9b511bb96033e596368519dd3cac5ed8d
d.n.warren <d.n.warren@5ee8cc34-b920-0410-bb03-fa7ad842d588>
2010-07-05 15:50:31 +0000

    Have altered PauseTest so that there is now more than 1 process enrolled on the 'SKIP' barrier.  Currently there is a total number of 20 processes.  19 of the processes alt over the 'pause' and 'skip' barriers, 1 only engages on the 'pause' barrier and only after 5 seconds.  Result no processes wait for the pause barrier to complete until after the 5 seconds are up at which point all processes choose the pause barrier (no deadlock).
    git-svn-id: http://projects.cs.kent.ac.uk/projects/jcsp/svn/jcsp/branches/dnw3_altbar@332 5ee8cc34-b920-0410-bb03-fa7ad842d588

--- src/org/jcsp/demos/altableBarriers/PauseTest.java
+++ src/org/jcsp/demos/altableBarriers/PauseTest.java
@@ -7,30 +7,39 @@ import org.jcsp.lang.*;
 //{{{ public class PauseTest
 public class PauseTest {
 
+	//{{{ contants
+	public static final int TOTAL_PROCESSES = 20;
+	//}}}
+
 	public static void main (String[] args) {
 		AltableBarrierBase pause = new AltableBarrierBase("\n\nPAUSE\n\n");
 		AltableBarrierBase skip = new AltableBarrierBase("SKIP");
+		CSProcess[] procs = new CSProcess[TOTAL_PROCESSES];
 
 		CSProcess tim = SampleProcesses.timProc(5000, pause);
-		GuardedCode pauseCode = new GuardedCode(pause,
-		new CSProcess() {
-			public void run() {
-				System.out.println("I have been paused");
-				System.out.println("I will die soon");
-				try {
-					Thread.sleep(5000);
-				} catch (Exception e) {}
-				System.out.println("about to die");
-				System.exit(0);
+		procs[0] = tim;
+		for (int i = 1; i < TOTAL_PROCESSES; i++) {
+			GuardedCode pauseCode = new GuardedCode(pause,
+			new CSProcess() {
+				public void run() {
+					System.out.println("I have been paused");
+					System.out.println("I will die soon");
+					try {
+						Thread.sleep(5000);
+					} catch (Exception e) {}
+					System.out.println("about to die");
+					System.exit(0);
+				}
 			}
+			);
+			GuardedCode skipCode = new GuardedCode(skip, new Skip());
+			CSProcess spin = SampleProcesses.guardedProc(
+			  new GuardedCode[] {pauseCode, skipCode}
+			);
+			procs[i] = spin;
 		}
-		);
-		GuardedCode skipCode = new GuardedCode(skip, new Skip());
-		CSProcess spin = SampleProcesses.guardedProc(
-		  new GuardedCode[] {pauseCode, skipCode}
-		);
 
-		Parallel par = new Parallel(new CSProcess[] {tim, spin});
+		Parallel par = new Parallel(procs);
 		par.run();
 		
 	}
