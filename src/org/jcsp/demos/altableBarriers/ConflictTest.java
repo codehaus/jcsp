f457830e0e237458eebf0bca259ec7dc67cae640
d.n.warren <d.n.warren@5ee8cc34-b920-0410-bb03-fa7ad842d588>
2010-10-22 13:58:31 +0000

    Added ConflictTest.java, this sets up a network of processes which have a priority conflict.  This syytem throws an ArrayIndexOutOfBoundsexception. Will investigate.
    git-svn-id: http://projects.cs.kent.ac.uk/projects/jcsp/svn/jcsp/branches/dnw3_altbar@357 5ee8cc34-b920-0410-bb03-fa7ad842d588

---
+++ src/org/jcsp/demos/altableBarriers/ConflictTest.java
@@ -0,0 +1,35 @@
+//{{{ package and import statements
+package org.jcsp.demos.altableBarriers;
+
+import org.jcsp.lang.*;
+//}}}
+
+//{{{ public class ConflictTest
+public class ConflictTest {
+
+	public static void main(String[] args) {
+		AltableBarrierBase a = new AltableBarrierBase();
+		AltableBarrierBase b = new AltableBarrierBase();
+
+		int as = 1;
+		int bs = 1;
+		AltableBarrierBase[] aFirst = new AltableBarrierBase[]{a,b};
+		AltableBarrierBase[] bFirst = new AltableBarrierBase[]{b,a};
+
+		CSProcess[] aProcs = new CSProcess[as];
+		CSProcess[] bProcs = new CSProcess[bs];
+
+		for (int i = 0; i < as; i++) {
+			aProcs[i] = SampleProcesses.priProc(aFirst);
+		}
+		for (int i = 0; i < bs; i++) {
+			bProcs[i] = SampleProcesses.priProc(bFirst);
+		}
+
+		(new Parallel(new CSProcess[]{
+			new Parallel(aProcs),
+			new Parallel(bProcs)
+		})).run();
+	}
+}
+//}}}
